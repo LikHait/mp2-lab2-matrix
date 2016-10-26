8295ae4fe52b0804e3646f4f2d0d833a3126f27e
LikHait <hait.2050@gmail.com>
2016-10-26 15:19:17 +0300

    fix

--- include/utmatrix.h
+++ include/utmatrix.h
@@ -62,8 +62,10 @@ public:
 template <class ValType>
 TVector<ValType>::TVector(int s, int si)
 {
-	if ((s < 1) || si < 0 || (s + si > MAX_VECTOR_SIZE))
-        throw(s, si);
+	if ((s < 1) || (s > MAX_VECTOR_SIZE))
+        throw(s);
+	if ((si < 0) || (si + s > MAX_VECTOR_SIZE))
+        throw(si);
 	Size = s;
 	StartIndex = si;
 	pVector = new ValType[s];
